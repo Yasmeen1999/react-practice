ddb253a79f9ce39f9a3d79531a256c1d926e3d81
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2019-08-04 14:49:46 +0530

    react-form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import LoginForm from "./LoginForm";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/about"} component={About} />
+          <Route exact path={"/contact"} component={Contact} />
+          <Route exact path={"/login"} component={LoginForm} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
